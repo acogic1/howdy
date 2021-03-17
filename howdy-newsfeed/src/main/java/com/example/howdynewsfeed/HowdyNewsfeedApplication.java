2724dba9415500100cd34d84f1c387114eee8a54
dzejlansabic <dzejlan.sabic@hotmail.com>
2021-03-17 01:04:48 +0100

    Initialize project

---
+++ howdy-newsfeed/src/main/java/com/example/howdynewsfeed/HowdyNewsfeedApplication.java
@@ -0,0 +1,13 @@
+package com.example.howdynewsfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowdyNewsfeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowdyNewsfeedApplication.class, args);
+	}
+
+}
