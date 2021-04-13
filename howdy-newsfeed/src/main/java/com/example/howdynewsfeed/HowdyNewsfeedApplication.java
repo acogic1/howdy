7773ecac781cb9eea48a7ed68c3bedf0efe8b8e2
dzejlansabic <dzejlan.sabic@hotmail.com>
2021-04-13 13:59:44 +0200

    Merge master to my branch

--- howdy-newsfeed/src/main/java/com/example/howdynewsfeed/HowdyNewsfeedApplication.java
+++ howdy-newsfeed/src/main/java/com/example/howdynewsfeed/HowdyNewsfeedApplication.java
@@ -12,13 +12,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import com.example.howdynewsfeed.repository.UserRepository;
+import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
 @EnableEurekaClient
 public class HowdyNewsfeedApplication {
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(HowdyNewsfeedApplication.class, args);
--- howdy-newsfeed/src/main/java/com/example/howdynewsfeed/controllers/PostController.java
+++ howdy-newsfeed/src/main/java/com/example/howdynewsfeed/controllers/PostController.java
@@ -3,17 +3,25 @@ package com.example.howdynewsfeed.controllers;
 import com.example.howdynewsfeed.Exceptions.InternalServerException;
 import com.example.howdynewsfeed.Exceptions.NotFoundException;
 import com.example.howdynewsfeed.models.Post;
+import com.example.howdynewsfeed.models.User;
 import com.example.howdynewsfeed.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping("api/")
 public class PostController {
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     //private final PostRepository postRepository;
     private final PostService postService;
@@ -30,6 +38,15 @@ public class PostController {
         catch (Exception e) {
             throw new InternalServerException();
         }
+    }
+
+    @GetMapping("postFollowing/{userId}")
+    List<User> getFollowingFromUser (@PathVariable("userId") Long id) {
+        ResponseEntity<User[]> responseEntity = restTemplate.getForEntity("localhost:8082/subscriptions/following/" + id, User[].class);
+
+        List<User> users = Arrays.asList(responseEntity.getBody());
+
+        return users;
     }
 
     @GetMapping("post/{id}")
