b31615660b41f9e4f789280f2854c48f49d49337
acogic1 <acogic1@etf.unsa.ba>
2021-04-14 14:46:15 +0200

    add proto file

--- system-events/src/main/java/com/example/systemevents/SystemEventsApplication.java
+++ system-events/src/main/java/com/example/systemevents/SystemEventsApplication.java
@@ -1,10 +1,18 @@
 package com.example.systemevents;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.IOException;
 
 @SpringBootApplication
 public class SystemEventsApplication {
+	private static final Logger log = LoggerFactory.getLogger(SystemEventsApplication.class);
+
+	@Autowired
+	private static SystemEventRepository systemEventRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SystemEventsApplication.class, args);
