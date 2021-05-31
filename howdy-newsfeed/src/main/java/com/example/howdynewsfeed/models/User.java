1146a2661c3ee9f3a194b4459b2ed2f8c991c10d
dzejlansabic <dzejlan.sabic@hotmail.com>
2021-05-31 23:03:25 +0200

    newsfeed changes

--- howdy-newsfeed/src/main/java/com/example/howdynewsfeed/models/User.java
+++ howdy-newsfeed/src/main/java/com/example/howdynewsfeed/models/User.java
@@ -28,11 +28,11 @@ public class User {
         this.username=username;
     }
 
-    /*public User (Long id, String username, String password) {
+    public User (Long id, String username, String password) {
         this.id=id;
         this.username=username;
         this.password=password;
-    }*/
+    }
 
     public Long getId() {
         return id;
